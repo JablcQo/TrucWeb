70c4b566b01fb2eb7aa33be5715153bf1a442b7a
JablcQo <trundafilip@gmail.com>
2020-10-23 23:12:04 +0200

    init commit

---
+++ src/main/java/com/trucWev/TrucWebApplication.java
@@ -0,0 +1,13 @@
+package com.trucWev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrucWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrucWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trucWev/TrucWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trucWev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrucWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
